40d1c3ca44ef117cb5929dd9df98fe4b18f2d274
MiSkynet <michele.sauer.2607@gmail.com>
2026-05-20 22:23:37 +0200

    interact entity listener

--- src/main/java/me/miskynet/customGamemode/custom/entitys/Listener.java
+++ src/main/java/me/miskynet/customGamemode/custom/entitys/Listener.java
@@ -1,9 +1,13 @@
 package me.miskynet.customGamemode.custom.entitys;
 
 import me.miskynet.customGamemode.Main;
+import me.miskynet.customGamemode.custom.Utils;
+import me.miskynet.customGamemode.custom.menu.Menu;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
 public class Listener implements org.bukkit.event.Listener {
@@ -11,15 +15,12 @@ public class Listener implements org.bukkit.event.Listener {
     @EventHandler
     public void entityClick(PlayerInteractAtEntityEvent event) {
 
-        NamespacedKey customEntityIdNamespaceKey = new NamespacedKey(Main.getInstance(), "custom_entity_id");
-        /*
-        * 26001 -> custom_entity_id set in the InteractEntity class
-        * */
-        if (event.getRightClicked().getPersistentDataContainer().get(customEntityIdNamespaceKey, PersistentDataType.INTEGER) == 26001) {
-            InteractEntity entity = (InteractEntity) event.getRightClicked();
-            entity.getInteractionMenu().openForPlayer(event.getPlayer());
+        PersistentDataContainer pdc = event.getRightClicked().getPersistentDataContainer();
+        NamespacedKey menuLink = new NamespacedKey(Main.getInstance(), "entity_menu_link");
+        int entityMenuId = pdc.get(menuLink, PersistentDataType.INTEGER);
+
+        if (Main.menuManager.searchId(entityMenuId) != null) {
+            Main.menuManager.searchId(entityMenuId).openForPlayer(event.getPlayer());
         }
-
     }
-
 }
