9f0a84ce44eeaa2e0ee0eee910fc307bed72a93d
MiSkynet <michele.sauer.2607@gmail.com>
2026-08-02 02:34:42 +0200

    GetLevel command The get level command (/level) is now a valid command and should work as intended

--- src/main/java/me/miskynet/customGamemode/commands/GetLevel.java
+++ src/main/java/me/miskynet/customGamemode/commands/GetLevel.java
@@ -6,12 +6,12 @@ import me.miskynet.customGamemode.Main;
 import me.miskynet.customGamemode.custom.config.Language;
 import me.miskynet.customGamemode.custom.index.levelingSystem.IndexLevelingSystem;
 import me.miskynet.customGamemode.utils.ComponentUtils;
-import me.miskynet.customGamemode.utils.PermsManager;
+import me.miskynet.customGamemode.utils.PermissionManager;
 import org.bukkit.entity.Player;
 
 public class GetLevel implements BasicCommand {
 
-    private IndexLevelingSystem indexLevelingSystem = Main.getInstance().getLevelingSystem();
+    private final IndexLevelingSystem indexLevelingSystem = Main.getInstance().getLevelingSystem();
     private final Language language = Main.getInstance().getLanguage();
 
     @Override
@@ -24,20 +24,29 @@ public class GetLevel implements BasicCommand {
 
         Player player = (Player) commandSourceStack.getSender();
 
-        if (!(player.hasPermission(PermsManager.Perms.COMMAND_GET_LEVEL.toLowerString())) && !player.isOp()) {
+        if (!(PermissionManager.Perm.COMMAND_PLAYER_GET_LEVEL.hasPermission(player)) && !player.isOp()) {
             player.sendMessage(ComponentUtils.component(language.getString("commands.general.noPermission")));
             return;
         }
 
         int nextLevel = indexLevelingSystem.getPlayerLevel(player) + 1;
 
+        if (args.length == 1) {
+            try {
+                nextLevel = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                player.sendMessage(ComponentUtils.component(language.getString("commands.level.invalidLevel")));
+                return;
+            }
+        }
+
         int requiredXPToLevelUp = indexLevelingSystem.getRequiredXPToLevelUp(nextLevel);
 
         player.sendMessage(ComponentUtils.component(language.getString("commands.level.currentLevel")
-                .replaceAll("%level%", indexLevelingSystem.getPlayerLevel(player) + "")
-                .replaceAll("%xp%", indexLevelingSystem.getPlayerXP(player) + "")
-                .replaceAll("%nextLevel%", nextLevel + "")
-                .replaceAll("%requiredXP%", requiredXPToLevelUp + "")));
+                .replace("%level%", indexLevelingSystem.getPlayerLevel(player) + "")
+                .replace("%xp%", indexLevelingSystem.getPlayerXP(player) + "")
+                .replace("%nextLevel%", nextLevel + "")
+                .replace("%requiredXP%", requiredXPToLevelUp + "")));
 
     }
 }
