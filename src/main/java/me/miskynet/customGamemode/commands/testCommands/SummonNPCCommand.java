a893ed215dd740813460dbebf120836bc985754e
MiSkynet <michele.sauer.2607@gmail.com>
2026-08-02 03:00:15 +0200

    Comments added for: - SummonNPCCommand - TexturedScrollMenu

--- src/main/java/me/miskynet/customGamemode/commands/testCommands/SummonNPCCommand.java
+++ src/main/java/me/miskynet/customGamemode/commands/testCommands/SummonNPCCommand.java
@@ -1,34 +1,49 @@
 package me.miskynet.customGamemode.commands.testCommands;
 import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
+import me.miskynet.customGamemode.Main;
+import me.miskynet.customGamemode.custom.config.Language;
 import me.miskynet.customGamemode.custom.entity.npc.NPC;
 import me.miskynet.customGamemode.utils.ComponentUtils;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public class SummonNPCCommand implements BasicCommand {
+
+    private final Language language = Main.getInstance().getLanguage();
+
     @Override
     public void execute(CommandSourceStack commandSourceStack, String[] args) {
 
         Player player = (Player) commandSourceStack.getSender();
+
+        if (commandSourceStack.getSender() instanceof Player && !player.isOp()) {
+            player.sendMessage(ComponentUtils.component(language.getString("commands.general.noPermission")));
+            return;
+        }
 
         player.sendMessage(ComponentUtils.component("\n&e&l[!] &r&eThis command is currently in testing state. It may not work as intended and " +
                 "may not support some features! Errors may be fixed in the future, else this command will be replaced by an official version!\n"));
 
         Location location = player.getLocation();
 
-        NPC npc = new NPC(ComponentUtils.component("&dCooler NPC"), location);
-
-        if (args.length > 0) {
-
-            String npcType = args[0];
-
-            npc = new NPC(ComponentUtils.component("&d" + npcType), location);
-
-            npc.setInteractMenu(NPC.InteractType.valueOf(npcType.toUpperCase()));
+        if (args.length == 0) {
+            player.sendMessage(ComponentUtils.component("&eUsage: /summonnpc <shopType> <displayName> <skinHash>"));
         }
 
-        npc.setSkinHash("de39ab9468b064c7c01aeddd345217b6dae58b51b37d9342b5a9bbf33f1197d5");
+        // get and set the custom name
+        String customName = (args.length >= 1) ? args[0] : "&dCustomNPC";
+
+        // create the NPC with the custom name and location
+        NPC npc = new NPC(ComponentUtils.component(customName), location);
+
+        // since currently, there is only one npc type, it will automatically be set to shop
+        // in case there will be more npc types in the future, this will change!
+        npc.setInteractMenu(NPC.InteractType.SHOP);
+
+        // get and set the texture hash, if not provided, use a default one
+        String textureHash = (args.length >= 3) ? args[2] : "de39ab9468b064c7c01aeddd345217b6dae58b51b37d9342b5a9bbf33f1197d5";
+        npc.setTextureHash(textureHash);
 
         npc.spawn();
 
--- src/main/java/me/miskynet/customGamemode/custom/menu/TexturedScrollMenu.java
+++ src/main/java/me/miskynet/customGamemode/custom/menu/TexturedScrollMenu.java
@@ -1,6 +1,8 @@
 package me.miskynet.customGamemode.custom.menu;
 
 import com.google.gson.JsonIOException;
+import me.miskynet.customGamemode.Main;
+import me.miskynet.customGamemode.custom.config.Language;
 import me.miskynet.customGamemode.custom.item.Item;
 import me.miskynet.customGamemode.custom.item.PlayerHead;
 import me.miskynet.customGamemode.utils.ComponentUtils;
@@ -22,6 +24,8 @@ public class TexturedScrollMenu extends TextureMenu {
 
     public static int nextPageButtonSlot = 4;
     public static int previousPageButtonSlot = 6;
+
+    private final Language language = Main.getInstance().getLanguage();
 
     /**
      * @param title   The title of the {@link TextureMenu}
@@ -47,11 +51,13 @@ public class TexturedScrollMenu extends TextureMenu {
     @Override
     public void buildMenu(Player player) {}
 
-
+    /**
+     * Adds the navigation bar to the {@link TextureMenu}. This includes the next and previous page buttons, as well as the current page indicator.
+     * */
     public void addNavigationBar() {
 
-        PlayerHead arrowLeft = new PlayerHead(ComponentUtils.component(false, "&ePrevious Page"), "528b8cf405eaf606a0210f0303b013179f8f12eaa95824129ebeef9e44b68230");
-        PlayerHead arrowRight = new PlayerHead(ComponentUtils.component(false, "&eNext Page"), "5dcda6e3c6dca7e9b8b6ba3febf5cd0917f997b64b2aef18c3f773765e3a579");
+        PlayerHead arrowLeft = new PlayerHead(ComponentUtils.component(false, language.getString("shop.item.previousPage")), "528b8cf405eaf606a0210f0303b013179f8f12eaa95824129ebeef9e44b68230");
+        PlayerHead arrowRight = new PlayerHead(ComponentUtils.component(false, language.getString("shop.item.nextPage")), "5dcda6e3c6dca7e9b8b6ba3febf5cd0917f997b64b2aef18c3f773765e3a579");
 
         /*
          * calculate if there is at least one item to fit in a new page
@@ -73,7 +79,8 @@ public class TexturedScrollMenu extends TextureMenu {
         }
 
         // item to present the current page
-        this.getInventory().setItem(this.getInventory().getSize() - 5, new Item(Material.BOOK, ComponentUtils.component(false, "&eCurrent Page: " + (this.currentPage + 1))).toItemStack());
+        String currentPageDisplayName = language.getString("shop.currentPage.displayName").replace("%currentPage%", String.valueOf(this.currentPage + 1));
+        this.getInventory().setItem(this.getInventory().getSize() - 5, new Item(Material.BOOK, ComponentUtils.component(false, currentPageDisplayName)).toItemStack());
 
         this.getInventory().setItem(this.getInventory().getSize() - 9, this.getInvisibleItem());
         this.getInventory().setItem(this.getInventory().getSize() - 8, this.getInvisibleItem());
@@ -83,40 +90,94 @@ public class TexturedScrollMenu extends TextureMenu {
         this.getInventory().setItem(this.getInventory().getSize() - 1, this.getInvisibleItem());
     }
 
+    /**
+     * Gets the current page of the {@link TexturedScrollMenu}
+     *
+     * @return Current page as {@link Integer}
+     * */
     public int getCurrentPage() {
         return this.currentPage;
     }
 
+    /**
+     * Sets the current page of the {@link TexturedScrollMenu}.
+     * To apply this change, you need to call the {@link #buildMenu()} function afterward for the {@link Player} to see the new current page
+     * */
     public void setCurrentPage(int currentPage) {
         this.currentPage = currentPage;
     }
 
+    /**
+     * Increases the current page of the {@link TexturedScrollMenu} by 1.
+     * To apply this change, you need to call the {@link #buildMenu()} function afterward for the {@link Player} to see the new current page
+     * */
     public void increasePage() {
-        // subtract one since the calculation of the max page is based on
-        // the number of items and not the index of the page and the current page is index based
+        // format the getMaxPage into index based, since the currentPage works by index
         if (this.currentPage < (this.getMaxPage() - 1)) this.currentPage++;
     }
 
+    /**
+     * Decreases the current page of the {@link TexturedScrollMenu} by 1.
+     * To apply this change, you need to call the {@link #buildMenu()} function afterward for the {@link Player} to see the new current page
+     * */
     public void decreasePage() {
         if (this.currentPage > 0) this.currentPage--;
     }
 
+    /**
+     * Set the max amount of pages for the {@link TexturedScrollMenu}. This is used to determine if the next page button should be displayed or not.
+     * To calculate the max page automatically, you can use the {@link #calculateMaxPage(int)} function, which will return the max page
+     *
+     * @param maxPage The max page as {@link Integer}
+     * */
     public void setMaxPage(int maxPage) {
         this.maxPage = maxPage;
     }
 
+    /**
+     * Calculates the max amount of pages for the {@link TexturedScrollMenu}
+     *
+     * @param itemTotal The total amount of items for the {@link TexturedScrollMenu} as {@link Integer}
+     * @return The max page as {@link Integer}
+     * */
+    public int calculateMaxPage(int itemTotal) {
+        return (itemTotal + getItemsPerPage() - 1) / getItemsPerPage();
+    }
+
+    /**
+     * Gets the max page of the {@link TexturedScrollMenu}
+     *
+     * @return Max page as {@link Integer}
+     * */
     public int getMaxPage() {
         return this.maxPage;
     }
 
+    /**
+     * Manually sets the amount of items per page for the {@link TexturedScrollMenu}. This is used to determine how many items should be displayed on each page,
+     * not including the navigation bar. The default value is the size of the {@link TextureMenu} minus 9, since the last row is reserved for the navigation bar.
+     * Do not set this value to a number higher than the size of the {@link TextureMenu} minus 9, as this will cause the navigation bar to be overwritten.
+     *
+     * @param itemsPerPage The amount of items per page as {@link Integer}
+     * */
     public void setItemsPerPage(int itemsPerPage) {
         this.itemsPerPage = itemsPerPage;
     }
 
+    /**
+     * Gets the amount of items per page for the {@link TexturedScrollMenu}
+     *
+     * @return Items per page as {@link Integer}
+     * */
     public int getItemsPerPage() {
         return this.itemsPerPage;
     }
 
+    /**
+     * Gets ab invisible item that can be used to fill empty slots in the {@link TexturedScrollMenu}.
+     *
+     * @return Invisible item as {@link ItemStack}
+     * */
     public ItemStack getInvisibleItem() {
         Item item = new Item(Material.GHAST_TEAR);
         ItemStack itemStack = item.toItemStack();
